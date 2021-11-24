355350a985e4beccd58ca6f622228fe85a5feb14
Emre Ayhan <Emre1999@hotmail.se>
2021-11-24 21:08:29 +0100

    WIP can navigate to post and message and

---
+++ Frontend/my-app/src/components/Post.js
@@ -0,0 +1,32 @@
+import { Form, Button, InputGroup,Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import React, { useState, useEffect } from 'react'
+import { deleteAllCookies, getCookie } from './Cookies';
+import { Alert } from 'bootstrap';
+import { Link, Navigate } from "react-router-dom";
+
+
+const Post = (props) =>{
+
+    console.log("fadebook username: "+props.userFade)
+    var val =getCookie(props.userFade);
+
+    console.log("coockie value: "+val)
+
+    return(
+        <form>
+        <label>
+        FadeBook UserName: {props.userFade}
+          <input type="text" value={val}  />
+          <input type="text"  defaultValue={val}  />
+        </label>
+       
+      </form>
+    )
+
+}
+
+
+export default Post
